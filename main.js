b4a24b0ca305ab23b4131e447e25fb4aef4fe56a
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2020-05-21 23:18:10 -0400

    report and proposal

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 2,
-  default_selection = "All";
+  default_selection = "None Selected";
 
 /** these variables allow us to access anything we manipulate in init() but need access to in draw().
  * All these variables are empty before we assign something to them.*/
@@ -17,7 +17,7 @@ let yScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selectedNeighborhood: "All",
+  selectedNeighborhood: "None Selected",
 };
 
 /**
